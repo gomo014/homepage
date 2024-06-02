3e48396d07ceaffd69acff012b95abfb27fb7e5b
doggydog <mtten2021@gmail.com>
2024-06-02 16:25:21 +0900

    finalComit

--- homepage/settings.py
+++ homepage/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = 'C:/Users/USER/Desktop/自習/homepage/homepage/app/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
