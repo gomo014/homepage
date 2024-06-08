bb6df1c2bb5618891b886d04801509eeed9c6511
doggydog <mtten2021@gmail.com>
2024-06-08 23:35:29 +0900

    aaa

--- homepage/settings.py
+++ homepage/settings.py
@@ -114,8 +114,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'homepage', 'app', 'static'),
 ]
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.ap-southeast-2.amazonaws.com'
